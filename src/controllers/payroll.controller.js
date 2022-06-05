535c2eecf0c2f27cec56f4f3c4d59cbb3546fd59
Joise Garcia <alejandrogar17@gmail.com>
2022-06-04 20:18:14 -0400

    feat: Store person to payroll

--- src/controllers/payroll.controller.js
+++ src/controllers/payroll.controller.js
@@ -37,3 +37,65 @@ export const index = async (req, res) => {
     rows: payrollModel,
   });
 };
+
+export const show = async (req, res) => {
+  const { id } = req.params;
+  const payrollModel = await PayrollModel.findOne({
+    where: {
+      cedula: id,
+    },
+    attributes: ["id", "cedula", "Nom_titular", "fecha_nacimiento", "edad"],
+    include: [
+      {
+        model: TypePayrollModel,
+        key: "id",
+        attributes: ["descripcion"],
+      },
+      {
+        model: MunicipalityModel,
+        key: "id",
+        attributes: ["descripcion"],
+      },
+      {
+        model: RegionModel,
+        key: "id",
+        attributes: ["descripcion"],
+      },
+    ],
+  });
+
+  res.status(200).json({
+    rows: payrollModel,
+  });
+};
+
+export const store = async (req, res) => {
+  try {
+    const {
+      typePayroll,
+      regionId,
+      municipalityId,
+      documentNumber,
+      fullName,
+      birthday,
+      age,
+    } = req.body;
+    const payrollModel = await PayrollModel.create({
+      typePayroll,
+      regionId,
+      municipalityId,
+      documentNumber,
+      fullName,
+      birthday,
+      age,
+    });
+
+    res.status(201).json({
+      rows: payrollModel,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
--- src/routes/payroll.routes.js
+++ src/routes/payroll.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { index } from "../controllers/payroll.controller.js";
+import { index, show, store } from "../controllers/payroll.controller.js";
 
 const router = Router();
 
 router.get("/", index);
+router.get("/show/:id", show);
+router.post("/store", store);
 
 export default router;
