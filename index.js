07f2f1c70092cefcc4e110ac87532c5180f002e8
Joise Garcia <alejandrogar17@gmail.com>
2022-06-05 03:36:24 -0400

    fix: dotenv dependecy

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ dotenv.config({
 });
 
 function main() {
-  app.listen(process.env.PORT, () => {
+  app.listen(process.env.PORT || "3000", () => {
     console.log(`Server is listening on port ${process.env.PORT}`);
   });
 }
